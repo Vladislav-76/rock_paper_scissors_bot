c234d1e5598a72ce47732d3f263948d6d03f94b5
Vladislav-76 <sillmaril2@gmail.com>
2023-11-03 14:11:46 +0300

    Final

---
+++ bot.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_data.config import BOT_TOKEN
+from handlers import other_handlers, user_handlers
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s'
+    )
+    logger.info('Starting bot')
+    bot = Bot(token=BOT_TOKEN, parse_mode='HTML')
+    dp = Dispatcher()
+    dp.include_routers(
+        user_handlers.router,
+        other_handlers.router
+    )
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
